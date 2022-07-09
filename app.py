24c012d01413dde096281d792fac00608a35825b
mattobrado <mcobradovic@gmail.com>
2022-07-09 10:06:34 -0500

    fix: added pycache to gitignore

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 from spotifyGroupChatSecrets import SECRET_KEY
 
 app = Flask(__name__) # Create Flask object
-app.config['SECRET_KEY'] = SECRET_KEY # SECRET_KEY imported from secrests.py
+app.config['SECRET_KEY'] = SECRET_KEY # SECRET_KEY for debug toolbar
 
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False # Disable intercepting redirects
 
