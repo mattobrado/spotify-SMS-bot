3228384cc9c72e54beb44cd1127110f92e8273d0
mattobrado <mcobradovic@gmail.com>
2022-07-09 10:01:39 -0500

    feat: initial flask app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from spotifyGroupChatSecrets import SECRET_KEY
+
+app = Flask(__name__) # Create Flask object
+app.config['SECRET_KEY'] = SECRET_KEY # SECRET_KEY imported from secrests.py
+
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False # Disable intercepting redirects
+
+toolbar = DebugToolbarExtension(app) # Create debug toolbar object
+
+@app.route('/')
+def root():
+  """Show homepage"""
+  return render_template("index.html")
