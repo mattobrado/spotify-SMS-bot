a462abdab4069bba97a84606359d52ed2df76bb7
mattobrado <mcobradovic@gmail.com>
2022-07-10 09:50:24 -0500

    refactor: cleaned up code

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def signup_user():
   form = UserForm() # Form for getting username and password
 
   if form.validate_on_submit():
-    username = form.username.data
+    username = form.username.data.lower() # make username all lowercase
     password = form.password.data
 
     new_user = User.register(username=username, password=password) # Create User object
@@ -44,45 +44,59 @@ def signup_user():
     try:
       db.session.commit()
     except IntegrityError:
-      form.username.errors.append('Username taken.')
+      form.username.errors.append('Username taken.') # Show an error on the form
       return render_template('users/signup.html', form=form) # Reload form
     
-    session['username'] = new_user.username #store username in session for authorization
-    flash('Welcome! Successfully create you account!', 'success') #
+    session['user_id'] = new_user.id #store user's id in session for authorization
+    flash('Welcome! Successfully create you account!', 'success')
     return redirect('/')
 
-  return render_template('users/signup.html', form=form)
+  else:
+    return render_template('users/signup.html', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_user():
-  """Login for user"""
-  form = UserForm()
+  """Get login form and handle input"""
+
+  form = UserForm() # Form for getting username and password
+  
   if form.validate_on_submit():
-    username = form.username.data
+    username = form.username.data.lower() # make username all lowercase
     password = form.password.data
-    user = User.authenticate(username=username, password=password)
+    user = User.authenticate(username=username, password=password) # Get User object
     
+    # If User.autheticate() returned a User object
     if user:
       flash('Logged in', 'success')
-      session['user_id'] = user.id
+      session['user_id'] = user.id # add user's id'to session
       return redirect('/playlists')
+
     else:
       form.username.errors = ['Invalid username/password']
-  return render_template('users/login.html',form=form)
+
+  return render_template('users/login.html',form=form) # Reload login form
+
 
 @app.route('/logout')
 def logout_user():
-  session.pop('user_id')
-  return redirect('/')
+  """Logout route"""
+
+  session.pop('user_id') # Remover user_id from session
+  return redirect('/login')
+
 
 @app.route('/playlists', methods=['GET', 'POST'])
 def show_playlists():
   """Show list of playlists"""
+
   if 'user_id' not in session:
     flash('Please log in', 'warning')
     return redirect('/')
-  form = PlaylistForm()
+
+  form = PlaylistForm() # Form for making a new playlist
   all_playlists = Playlist.query.all()
+
   if form.validate_on_submit():
     title = form.title.data
     new_playlist = Playlist(title=title, user_id=session['user_id'])
@@ -93,15 +107,20 @@ def show_playlists():
   
   return render_template('playlists.html', form=form, playlists=all_playlists)
 
+
 @app.route('/playlists/<int:id>/delete', methods=['POST'])
 def delete_playlist(id):
   """Delete a playlists"""
+
   playlist = Playlist.query.get_or_404(id)
   if playlist.user_id == session['user_id']:
     db.session.delete(playlist)
     db.session.commit()
     flash('Playlist deleted')
     return redirect('/playlists')
-  flash('You cannot delete that playlist')
+  
+  # User was not authorized to delete that playlist
+  else:
+    flash('You cannot delete that playlist')
 
   return redirect('/playlists')
