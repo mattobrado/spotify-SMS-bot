e7d86f47be988c79ae25cd1ac7a9aa6f6b12de68
mattobrado <mcobradovic@gmail.com>
2022-08-07 10:07:12 -0500

    fix: removed print statement

--- spotify.py
+++ spotify.py
@@ -128,7 +128,6 @@ def get_or_create_host_user(auth_data):
   USER_PROFILE_ENDPOINT = SPOTIFY_API_URL + '/me'
   profile_response = requests.get(USER_PROFILE_ENDPOINT, headers=auth_header) # .json() to unpack
 
-  print(f"profile_response.status_code {profile_response.status_code}")
   # If we got 403 "forbidden" (if the spotify account is not added to our app, required because the spotify app is in development mode) 
   if profile_response.status_code == 403: 
     return None # Don't return a host_user.
